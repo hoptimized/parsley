b939d732ba6e88281dc19e292d72c3ab6e1f2beb
Tim Hopp <57064437+hoptimized@users.noreply.github.com>
2026-07-30 21:38:05 +0200

    implement simple StringView

--- include/parsley/core/StringView.h
+++ include/parsley/core/StringView.h
@@ -1,8 +1,35 @@
 #pragma once
 
-#include <string_view>
+#include <cstring>
+#include <string>
 
 namespace parsley
 {
-    using StringView = std::string_view;
+    class StringView
+    {
+        using CharT = char;
+
+    public:
+        StringView(const CharT* str) : ptr_(str), len_(std::strlen(str)) {}
+        StringView(const std::string& str) : ptr_(str.data()), len_(str.size()) {}        
+
+        operator std::string() const
+        {
+            return std::string{ ptr_, len_ };
+        }
+
+        bool operator==(const std::string& str)
+        {
+            if (len_ != str.size())
+                return false;
+
+            return std::memcmp(ptr_, str.data(), std::min(len_, str.size())) == 0;
+        }
+
+    private:
+        const CharT* ptr_;
+        size_t len_;
+    };
+
+    inline bool operator==(const std::string& lhs, const StringView& rhs) { return rhs == lhs; }
 }
